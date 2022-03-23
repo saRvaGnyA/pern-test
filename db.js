06db7fc97e6c7805f1a0d507eacd3f30dd5e107f
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-03-24 00:07:00 +0530

    production push

--- db.js
+++ db.js
@@ -2,12 +2,20 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 // instantiate this Pool
-const pool = new Pool({
+const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PW,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DB,
-});
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
